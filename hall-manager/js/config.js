e9c8c64d73f8c5e729a02524debf4efb0bbba86f
martinsdominic1 <martinsdominic1@gmail.com>
2026-09-16 08:06:39 +0200

    Add files via upload

--- hall-manager/js/config.js
+++ hall-manager/js/config.js
@@ -1,2 +1,2 @@
 // Paste the deployed Apps Script Web App URL here (see SETUP.md).
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxKFNye7GTtB1oQbQ5IplMSaYCKFIkP0-tk09Lhnt71-T9uykApiBwMTYfaS4F0l8wF/exec";
